d7c960f1d4d27398668bb7361bcef0b8eee3c9b0
likoli007 <holubek.alojz@gmail.com>
2023-05-31 09:36:05 +0800

    Initial Commit

---
+++ ARGameUITests/ARGameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ARGameUITestsLaunchTests.swift
+//  ARGameUITests
+//
+//  Created by Alojz on 2023/5/31.
+//
+
+import XCTest
+
+final class ARGameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
